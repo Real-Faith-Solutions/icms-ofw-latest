d3c8d6788288084723326391719ef3b7f709185a
lightdcreator <hilomafrancisoliver@gmail.com>
2024-05-23 11:49:15 +0800

    Step 4 Incident Detail's  Place of Incident field saving succesfull into database work collaboration with sir Jhunriz

--- public_html/assets/modules/administrator/js/sub_add_case/case_details.js
+++ public_html/assets/modules/administrator/js/sub_add_case/case_details.js
@@ -2657,7 +2657,8 @@ function storeCaseDetails() {
         'complainant_relation_other': $('.case-complainant_relation_other').val(),
         'complainant_address': $('.case-complainant_address').val(),
         'complainant_contact': $('.case-complainant_contact').val(),
-        'complainant_remarks': $('.case-complainant_remarks').val()
+        'complainant_remarks': $('.case-complainant_remarks').val(),
+        'complainant_place_of_incident': $('.case-complainant_place_of_incident').val()
     };
 
     //Acts
