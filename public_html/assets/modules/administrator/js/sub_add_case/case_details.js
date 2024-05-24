c11bae254af8f146e1a626a7756cb0ba607e8f26
hilomafrancisoliver <hilomafrancisoliver@gmail.com>
2024-05-24 11:40:31 +0800

    Step 4 Incident Detail's Offender POB  successfull saving into database  work collaboration with sir Jhunriz

--- public_html/assets/modules/administrator/js/sub_add_case/case_details.js
+++ public_html/assets/modules/administrator/js/sub_add_case/case_details.js
@@ -2833,7 +2833,8 @@ function storeCaseOffender() {
                 'offender_address': $('.a-case-offender_address').val(),
                 'offender_contact': $('.a-case-offender_contact').val(),
                 'offender_remarks': $('.a-case-offender_remarks').val(),
-                'offender_alias': $('.a-case-offender_alias').val()
+                'offender_alias': $('.a-case-offender_alias').val(),
+                'offender_pob': $('.a-case-offender_place_of_birth').val()
             }];
 
         _setStorageData(victim_case_offender, 'victim_case_offender');
