2629fbfbfcb9fd5fe29786a14670722e8f74588b
hilomafrancisoliver <hilomafrancisoliver@gmail.com>
2024-05-30 04:07:00 +0800

    Step 4 Incident Detail's UPDATE Place of Origin  field successfull save into database and fetch to front-end

--- public_html/application/views/administrator/content/sub_update_case/case_details.php
+++ public_html/application/views/administrator/content/sub_update_case/case_details.php
@@ -82,7 +82,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                         <div class="row">
                             <div class="form-group col-lg-12 col-md-12 col-sm-12">
                                 <label>Place of Origin</label>
-                                <textarea class="form-control valid noSpcStart case-complainant_place_of_origin" rows="4" maxlength="100" aria-invalid="false"></textarea>
+                                <textarea class="form-control valid noSpcStart case-complainant_place_of_origin" id="case-complainant_place_of_origin" rows="4" maxlength="100" aria-invalid="false"></textarea>
                             </div>
                         </div>
                         <div class="row">
